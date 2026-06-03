736a837e3066dc219e9fc0a98dcdd2475f15c0b1
sidvar <gce.jorge@gmail.com>
2026-06-03 14:33:27 -0600

    Se agrega la resolucion del ejercicio 9

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+const sumAll = function(inicio, fin) {
+    if ((inicio > 0 && fin > 0) && (Number.isInteger(inicio) && Number.isInteger(fin))){
+        if (inicio < fin){
+            let Resultado = 0;
+            for (let i = inicio; i <= fin; i++){
+                Resultado += i;
+            }
+            return Resultado;
+        } else {
+            let Resultado = 0;
+            for (let i = fin; i <= inicio; i++){
+                Resultado += i;
+            }
+            return Resultado;
+        }
+    } else {
+        return 'ERROR'
+    }
+    // return inicio.isInteger()
 
 };
 
